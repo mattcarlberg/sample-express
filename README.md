bbfe33025786a4ba38780ae92ec44f940979b889
MAC <mcarlberg@bhsec.bard.edu>
2024-07-02 20:59:21 -0400

    express node.js server based on Chapter 1 of Complete Developer

---
+++ README.md
@@ -0,0 +1,3 @@
+# A Basic Server in node.js Using Express
+
+## Following along with The Complete Developer book by Martin Krause 
---
+++ index.js
@@ -0,0 +1,12 @@
+const express = require('express')
+const server = express() 
+const port = 3000 
+
+server.get('/hello', function (req, res) {
+	res.send("Hello world!"); 
+}); 
+
+server.listen(port, function() {
+	console.log(`listening on port ${port}`)
+}); 
+
